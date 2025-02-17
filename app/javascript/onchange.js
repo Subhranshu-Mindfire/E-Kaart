e1a7236716e2f3cef1dcd841b99eb1fb3574471f
subhranshu <subhranshu.mfs@gmail.com>
2025-02-17 14:37:13 +0530

    Onchange of status form get submitted in orders status

--- app/javascript/onchange.js
+++ app/javascript/onchange.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("order_item_status").onchange=function(e){
-  document.getElementById("frm").submit();
-}
+    document.getElementById("frm").submit();
+  }
 })
